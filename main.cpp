ecfaca49c8a5ecfd7e70d563292191face4e6eba
Francis Dinh <francisdinh@outlook.com>
2016-12-05 14:48:16 -0800

    Added profiling with gprof options to makefile

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main() {
 
     // set board to initial position
     Board b("2r1r3/pp1n1k2/3B1p2/2pp1p1p/2P2P1P/6P1/PP2R1K1/4R3 w - - 1 8");
-    std::cout << negamax(b, 6) << std::endl;
+    std::cout << negamax(b, 5) << std::endl;
 
     /*
     // main game loop
--- makefile
+++ makefile
@@ -4,5 +4,8 @@ default:
 debug:
 	g++ -std=c++11 -Wall -g -o SuperSnek main.cpp board.cpp
 
+profile:
+	g++ -std=c++11 -Wall -O2 -pg -o SuperSnek main.cpp board.cpp
+
 clean: SuperSnek
 	rm SuperSnek
